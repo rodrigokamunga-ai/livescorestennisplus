4c91ba6a321f8311f273fce73199f13ec867ac77
rodrigokamunga-ai <rodrigokamunga@gmail.com>
2026-06-14 14:39:34 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,91 @@
+const CACHE_NAME = "live-scores-tennis-plus-v1";
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "./login.html",
+  "./register.html",
+  "./reset-password.html",
+  "./menu.html",
+  "./admin.html",
+  "./player.html",
+  "./carreira.html",
+  "./dashboard.html",
+  "./perfil.html",
+  "./public.html",
+  "./users-admin.html",
+  "./manifest.json",
+  "./css/style.css",
+  "./css/menu.css",
+  "./css/perfil.css",
+  "./css/player.css",
+  "./css/carreira.css",
+  "./css/admin.css",
+  "./css/users-admin.css",
+  "./css/dashboard.css",
+  "./js/firebase.js",
+  "./js/login.js",
+  "./js/register.js",
+  "./js/reset-password.js",
+  "./js/menu.js",
+  "./js/perfil.js",
+  "./js/player.js",
+  "./js/tennisRules.js",
+  "./js/carreira.js",
+  "./js/admin.js",
+  "./js/users-admin.js",
+  "./js/dashboard.js",
+  "./js/public.js",
+  "./img/icon-192.png",
+  "./img/icon-512.png",
+  "./img/logo.png",
+  "./img/avatar-default.png",
+  "./img/perfil-padrao.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          return networkResponse;
+        })
+        .catch(() => {
+          if (event.request.mode === "navigate") {
+            return caches.match("./login.html");
+          }
+        });
+    })
+  );
+});
